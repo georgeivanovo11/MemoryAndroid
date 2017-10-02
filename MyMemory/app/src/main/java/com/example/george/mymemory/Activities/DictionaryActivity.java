da311dfee04b3d7dd397106df76b8a990fac816b
george <george-mamardashvili@yandex.ru>
2017-10-02 23:39:36 +0300

    dictionary xml

--- MyMemory/app/src/main/java/com/example/george/mymemory/Activities/DictionaryActivity.java
+++ MyMemory/app/src/main/java/com/example/george/mymemory/Activities/DictionaryActivity.java
@@ -2,16 +2,27 @@ package com.example.george.mymemory.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.george.mymemory.R;
 
 public class DictionaryActivity extends AppCompatActivity
 {
+    ListView mSegmentsListView;
+
+    String[] countries = {"asd","asd","sadas"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
+
+        mSegmentsListView = (ListView) findViewById(R.id.segments_list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,countries);
+
+        mSegmentsListView.setAdapter(adapter);
     }
 }
--- MyMemory/app/src/main/java/com/example/george/mymemory/Activities/ProfileActivity.java
+++ MyMemory/app/src/main/java/com/example/george/mymemory/Activities/ProfileActivity.java
@@ -1,17 +1,31 @@
 package com.example.george.mymemory.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.george.mymemory.R;
 
 public class ProfileActivity extends AppCompatActivity
 {
+    Button mDictionaryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mDictionaryButton = (Button) findViewById(R.id.dictionary_button);
+
+        mDictionaryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent DictionaryIntent = new Intent(ProfileActivity.this, DictionaryActivity.class);
+                startActivity(DictionaryIntent);
+            }
+        });
     }
 }
