ee0f757fcf0729ac1f15ef14aee92f63181d67f1
george <george-mamardashvili@yandex.ru>
2017-10-02 23:15:10 +0300

    profile activity view

---
+++ MyMemory/app/src/main/java/com/example/george/mymemory/Activities/DictionaryActivity.java
@@ -0,0 +1,17 @@
+package com.example.george.mymemory.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.george.mymemory.R;
+
+public class DictionaryActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dictionary);
+    }
+}
---
+++ MyMemory/app/src/main/java/com/example/george/mymemory/datatry.java
@@ -0,0 +1,8 @@
+package com.example.george.mymemory;
+
+/**
+ * Created by george on 02.10.17.
+ */
+
+public class datatry {
+}
